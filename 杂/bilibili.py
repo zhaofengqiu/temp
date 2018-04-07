54a38fc8110c0d049a682d57469339caad45ad0a
zhaofengqiu <36105719+zhaofengqiu@users.noreply.github.com>
2018-04-07 21:39:05 +0800

    Update bilibili.py

--- 杂/bilibili.py
+++ 杂/bilibili.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import requests
-import json
 from lxml import etree
 from bs4 import BeautifulSoup
 import time
@@ -14,15 +13,6 @@ A=Adsl()
 a=10
 user=['Mozilla/5.0(Macintosh;IntelMacOSX10.6;rv:2.0.1)Gecko/20100101Firefox/4.0.1',
       'Mozilla/5.0(WindowsNT6.1;rv:2.0.1)Gecko/20100101Firefox/4.0.1',
-      'Opera/9.80(Macintosh;IntelMacOSX10.6.8;U;en)Presto/2.8.131Version/11.11',
-      'Opera/9.80(WindowsNT6.1;U;en)Presto/2.8.131Version/11.11',
-      'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11',
-      'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;TencentTraveler4.0)',
-      'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1)',
-      'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;TheWorld)',
-      'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;Trident/4.0;SE2.XMetaSr1.0;SE2.XMetaSr1.0;.NETCLR2.0.50727;SE2.XMetaSr1.0)',
-      'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;360SE)',
-      'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;AvantBrowser)',
       'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1)',
       'Mozilla/5.0(BlackBerry;U;BlackBerry9800;en)AppleWebKit/534.1+(KHTML,likeGecko)Version/6.0.0.337MobileSafari/534.1+',
       'Mozilla/4.0(compatible;MSIE8.0;WindowsNT6.0;Trident/4.0)']
